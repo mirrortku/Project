5f070aebc9ced687dbfbb90cba5e70556626cc1c
C-programming <37432589+mirrortku@users.noreply.github.com>
2018-12-21 15:45:28 +0800

    Update main.cpp

--- menu/main.cpp
+++ menu/main.cpp
@@ -1,1 +1,21 @@
+#include "magic.h"
+
+int main(){
+  int m[MAX][MAX]={0};
+  int N;
+
+  printf("Enter N: ");
+  scanf("%d",&N);
+  for(int pos=1;pos<=4;pos++){
+    for(int type=1;type<=2;type++) {
+      reset(m);
+      magic(m,type,N,pos);
+      prmagic(m,N);
+      printf("\n");
+      compSum(m,N);
+      prmagicSum(m,N+1);
+      printf("\n\n");
+    }
+  }
+}
 
